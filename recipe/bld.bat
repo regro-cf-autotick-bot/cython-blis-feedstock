1302cc680d1c7185f91e6c895797ecfeef49fc32
H. Vetinari <h.vetinari@gmx.com>
2025-11-16 17:33:22 +1100

    yet another path to consider

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,5 +2,6 @@ set "PATH=C:\Program Files\LLVM\bin;%PATH%"
 set "INCLUDE=%$VC_INCLUDEPATH%"
 :: delete existing LLVM setup in image that often gets higher precedence
 rmdir /s /q "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm"
+rmdir /s /q "C:\Program Files\LLVM\bin"
 clang --version
 %PYTHON% -m pip install . --no-deps -vvv
