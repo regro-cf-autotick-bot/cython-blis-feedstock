0c3702a013a16e1089f3ceea6cfd8717258e981c
H. Vetinari <h.vetinari@gmx.com>
2025-11-16 17:23:23 +1100

    remove clang from image

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,6 @@
 set "PATH=C:\Program Files\LLVM\bin;%PATH%"
 set "INCLUDE=%$VC_INCLUDEPATH%"
+:: delete existing LLVM setup in image that often gets higher precedence
+rmdir /s /q "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm"
 clang --version
 %PYTHON% -m pip install . --no-deps -vvv
